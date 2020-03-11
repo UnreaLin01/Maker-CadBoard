882a8a587ff28516e1d705fd9d3261793361c73a
UnreaLin <53108504+UnreaLin01@users.noreply.github.com>
2020-03-11 23:00:39 +0800

    Prototype v1

--- Maker-CadBoard/Maker-CadBoard.ino
+++ Maker-CadBoard/Maker-CadBoard.ino
@@ -1,4 +1,7 @@
-#include "Keyboard.h"
+#define Mux1A A3
+#define Mux1B A4
+#define Mux1C A5
+#define Mux1Input 2
 
 boolean preSw1 = false;
 boolean preSw2 = false;
@@ -8,98 +11,32 @@ boolean preSw5 = false;
 boolean preSw6 = false;
 boolean preSw7 = false;
 boolean preSw8 = false;
-boolean preSw9 = false;
-boolean preSw10 = false;
 
-long count = 0;
 
 void setup() {
   Serial.begin(115200);
-  Keyboard.begin();
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,INPUT_PULLUP);
-  pinMode(4,INPUT_PULLUP);
-  pinMode(5,INPUT_PULLUP);
-  pinMode(6,INPUT_PULLUP);
-  pinMode(A5,INPUT_PULLUP);
-  pinMode(A4,INPUT_PULLUP);
-  pinMode(A3,INPUT_PULLUP);
-  pinMode(A2,INPUT_PULLUP);
-  pinMode(A1,INPUT_PULLUP);
+  
+  pinMode(Mux1A,OUTPUT);
+  pinMode(Mux1B,OUTPUT);
+  pinMode(Mux1C,OUTPUT);
+  pinMode(Mux1Input,INPUT);
 }
 
 void loop() {
-  boolean sw1 = digitalRead(2);
-  boolean sw2 = digitalRead(3);
-  boolean sw3 = digitalRead(4);
-  boolean sw4 = digitalRead(5);
-  boolean sw5 = digitalRead(6);
-  boolean sw6 = digitalRead(A5);
-  boolean sw7 = digitalRead(A4);
-  boolean sw8 = digitalRead(A3);
-  boolean sw9 = digitalRead(A2);
-  boolean sw10 = digitalRead(A1);
-  
-  if((preSw1 == HIGH) && (sw1 == LOW)){
-    Keyboard.press('1');
-  }else if((preSw1 == LOW) && (sw1 == HIGH)){
-    Keyboard.release('1');
-  }
-  if((preSw2 == HIGH) && (sw2 == LOW)){
-    Keyboard.press('2');
-  }else if((preSw2 == LOW) && (sw2 == HIGH)){
-    Keyboard.release('2');
-  }
-  if((preSw3 == HIGH) && (sw3 == LOW)){
-    Keyboard.press('3');
-  }else if((preSw3 == LOW) && (sw3 == HIGH)){
-    Keyboard.release('3');
-  }
-  if((preSw4 == HIGH) && (sw4 == LOW)){
-    Keyboard.press('4');
-  }else if((preSw4 == LOW) && (sw4 == HIGH)){
-    Keyboard.release('4');
-  }
-  if((preSw5 == HIGH) && (sw5 == LOW)){
-    Keyboard.press('5');
-  }else if((preSw5 == LOW) && (sw5 == HIGH)){
-    Keyboard.release('5');
-  }
-  
-  if((preSw6 == HIGH) && (sw6 == LOW)){
-    Keyboard.press('6');
-  }else if((preSw6 == LOW) && (sw6 == HIGH)){
-    Keyboard.release('6');
-  }
-  if((preSw7 == HIGH) && (sw7 == LOW)){
-    Keyboard.press('7');
-  }else if((preSw7 == LOW) && (sw7 == HIGH)){
-    Keyboard.release('7');
-  }
-  if((preSw8 == HIGH) && (sw8 == LOW)){
-    Keyboard.press('8');
-  }else if((preSw8 == LOW) && (sw8 == HIGH)){
-    Keyboard.release('8');
-  }
-  if((preSw9 == HIGH) && (sw9 == LOW)){
-    Keyboard.press('9');
-  }else if((preSw9 == LOW) && (sw9 == HIGH)){
-    Keyboard.release('9');
-  }
-  if((preSw10 == HIGH) && (sw10 == LOW)){
-    Keyboard.press(KEY_RETURN);
-  }else if((preSw10 == LOW) && (sw10 == HIGH)){
-    Keyboard.release(KEY_RETURN);
-  }
+  Serial.print(readMux1(0, 0, 0));
+  Serial.print(readMux1(0, 0, 1));
+  Serial.print(readMux1(0, 1, 0));
+  Serial.print(readMux1(0, 1, 1));
+  Serial.print(readMux1(1, 0, 0));
+  Serial.print(readMux1(1, 0, 1));
+  Serial.print(readMux1(1, 1, 0));
+  Serial.print(readMux1(1, 1, 1));
+  Serial.println();
+}
 
-  preSw1 = sw1;
-  preSw2 = sw2;
-  preSw3 = sw3;
-  preSw4 = sw4;
-  preSw5 = sw5;
-  preSw6 = sw6;
-  preSw7 = sw7;
-  preSw8 = sw8;
-  preSw9 = sw9;
-  preSw10 = sw10;
+boolean readMux1(boolean c,boolean b, boolean a){
+  digitalWrite(Mux1A,a);
+  digitalWrite(Mux1B,b);
+  digitalWrite(Mux1C,c);
+  return digitalRead(Mux1Input);
 }
